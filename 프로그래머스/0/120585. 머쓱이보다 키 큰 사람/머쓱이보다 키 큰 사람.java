525765e2375522ac2340b8ad0223f3c464c476d4
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-09 00:26:24 +0900

    Update 머쓱이보다 키 큰 사람.java

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -9,3 +9,25 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 방식
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int i : array) {
+            if(i > height) answer++;
+        }
+        return answer;
+    }
+}
+
+
+
+//스트림 방식
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        return (int) Arrays.stream(array).filter(value -> value > height).count();
+    }
+}
