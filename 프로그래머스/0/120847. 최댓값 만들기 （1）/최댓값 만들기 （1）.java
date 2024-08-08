ec6c44f2a12acbab4d1836200f3e296eadf261aa
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-09 01:25:18 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.51 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        //오름차순으로 정렬
+        Arrays.sort(numbers);
+        
+        //배열 길이 변수지정
+        int n = numbers.length;
+        
+        //가장 높은 값과 그 밑 값 곱
+        answer = numbers[n-1]*numbers[n-2];
+        
+        return answer;
+    }
+}
