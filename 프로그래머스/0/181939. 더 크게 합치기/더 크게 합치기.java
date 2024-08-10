e0c51783fd067d29c2eee4e7c633f8e38049505f
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-10 22:03:29 +0900

    Update 더 크게 합치기.java

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -11,11 +11,20 @@ class Solution {
     }
 }
 
-// 다른 방법
-// class Solution {
-//     public int solution(int a, int b) {
-//         String answer = ""+a+b;
-//         String answer2 = ""+b+c;
-//         return answer.compareTo(answer2)>=1 ?Integer.parsInt(answer):Integer.parsInt(answer2);
-//     }
-// }
+ 다른 방법
+ class Solution {
+    public int solution(int a, int b) {
+        String answer = ""+a+b;
+        String answer2 = ""+b+c;
+            answer = answer > answer2 ? answer : answer2;
+         return answer;
+     }
+ }
+
+// 더 간편한 방법
+Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a))
+class Solution {
+    public int solution(int a, int b) {
+        return Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a));
+    }
+}
