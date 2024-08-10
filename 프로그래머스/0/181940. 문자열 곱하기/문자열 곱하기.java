6bd4f7b21fcb7ab0f05247ebef6d4b4b5dc62b2a
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-10 21:32:01 +0900

    Update 문자열 곱하기.java

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -7,3 +7,13 @@ class Solution {
         return answer;
     }
 }
+
+// 간편한 방법
+
+class Solution {
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+}
+
+//repeat()을 쓰면 반복할 수 있다
