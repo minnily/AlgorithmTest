7db9f04d0259b31b559b417e80c33c38e4cfd5ce
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-12 23:55:31 +0900

    Update 등차수열의 특정한 항만 더하기.java

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -10,3 +10,12 @@ class Solution {
         return answer;
     }
 }
+
+//스트림으로 풀기
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        return IntStream.range(0, included.length).map(idx -> included[idx]?a+(idx*d):0).sum();
+    }
+}
