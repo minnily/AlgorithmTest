9b0d610de4ff8e1c6eca7a8c46d8b86e7aab2f84
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-12 23:55:08 +0900

    Update 코드 처리하기.java

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -2,8 +2,7 @@ class Solution {
     public String solution(String code) {
         StringBuilder answer = new StringBuilder();
         int mode = 0;
-        
-        
+
         for(int idx = 0; idx<code.length();idx++){
           if(code.charAt(idx)=='1') {
               mode= (mode==0) ? 1:0;
@@ -16,4 +15,25 @@ class Solution {
         }
              return answer.length() == 0? "EMPTY":answer.toString();
    }
+}
+
+// 다른 방법 
+class Solution {
+    public String solution(String code) {
+        StringBuilder answer = new StringBuilder();
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            }
+
+            //홀수 짝수인 경우를 mode로 표현할 수 있다니..!
+            if (i % 2 == mode) {
+                answer.append(current);
+            }
+        }
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
+    }
 }
