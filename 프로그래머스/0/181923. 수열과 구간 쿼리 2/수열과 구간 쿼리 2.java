65e2370ecbefa7663b245b5530c41b9ad6c38612
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-15 21:01:04 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.72 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        int idx = 0;
+        
+        for(int[] query:queries){
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            
+            int min = Integer.MAX_VALUE;
+            boolean flag = false;
+            
+            for(int i=s; i<=e;i++){
+                if(arr[i]>k && arr[i] < min){
+                    min = arr[i];
+                    flag=true;
+                }
+            }
+            if(flag){
+                answer[idx] = min;
+            }else{
+                answer[idx] = -1;
+            }
+            idx++;
+            
+        }
+        return answer;
+    }
+}
