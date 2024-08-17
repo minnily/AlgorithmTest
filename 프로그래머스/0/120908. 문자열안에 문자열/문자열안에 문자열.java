cf859f4ac9a2a3199fe8e71726330d0cf5c049a9
minkyung <156267800+minnily@users.noreply.github.com>
2024-08-17 17:35:51 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.06 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,22 +1,13 @@
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i =0;i<=str1.length();i++){
-            for(int j=0; j<=str2.length();j++){
-                if(str1.contains(str2)){
-                    answer = 1;
-                }else{
-                    answer =2;
-                }
-            }
-            
+
+        if(str1.contains(str2)){
+            answer = 1;
+        }else{
+            answer =2;
         }
-        
-        
-        
+
         return answer;
     }
 }
